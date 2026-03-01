1f4d5b32559fc8e9d14d8b8a3c7b4af0dd7a3464
Rayner Mora <morarayner797@gmail.com>
2026-03-01 19:54:54 -0400

    Detallare parte por parte este commit:
    1. Agregue los comentarios que no tenia mi código anterior mente, para mayor legibilidad y así seguiré añadiendo.
    
    2. Definí las variable del movimiento del jugador.
    
    3. Coloque el tiempo para poder controlar los FPS.
    
    4. Calcule el movimiento del jugador.
    
    5. Coloque la opción para poder mover jugador y haga efecto cuando dejemos de presionar esa tecla la cual mueve el jugador.
    
    6. Agregue Nuevas constantes color_personaje, color_Fondo, FPS, VELOCIDAD.

--- constantes.py
+++ constantes.py
@@ -1,5 +1,17 @@
+# Este archivo contiene las constantes utilizadas en el juego, como el tamaño de la ventana, el tamaño del personaje su color y demas caracteres añadidas. Esto permite una fácil modificación de estos valores sin tener que buscar en todo el código.
+
 ANCHO_VENTANA = 800
+
 ALTO_VENTANA = 600
-ANCHO_PERSONAJE = 30
-ALTO_PERSONAJE = 80
+
+ANCHO_PERSONAJE = 20
+
+ALTO_PERSONAJE = 20
+
 COLOR_PERSONAJE = (255, 255, 0)
+
+COLOR_FONDO = (0, 0, 20)
+
+FPS = 60
+
+VELOCIDAD = 5
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#Aqui importe constantes y personaje para mejor facilidad y manejo del codigo.
+
 import pygame
 import constantes
 from personaje import personaje
+
+#Inicializacion del personaje, ventana del juego y nombre.
 
 jugador = personaje(x=50, y=50)
 
@@ -10,15 +14,76 @@ ventana = pygame.display.set_mode((constantes.ANCHO_VENTANA, constantes.ALTO_VEN
 
 pygame.display.set_caption("SHOOT")
 
+#Definir las variables del movimiento, jugador.
+mover_derecha = False
+mover_izquierda = False
+mover_arriba = False
+mover_abajo = False
+
+#Controlar los fragmentos por segundo
+
+reloj = pygame.time.Clock()
+
 run = True
 while run == True:
 
+    #Especificar la velocidad
+    reloj.tick(constantes.FPS)
+
+    
+    ventana.fill(constantes.COLOR_FONDO)
+
+    #Calcular movimiento del jugador.
+
+    delta_x = 0
+    delta_y = 0
+
+    if mover_derecha == True:
+        delta_x = constantes.VELOCIDAD
+    if mover_izquierda == True:
+        delta_x = -constantes.VELOCIDAD
+    if mover_arriba == True:
+        delta_y = -constantes.VELOCIDAD
+    if mover_abajo == True:
+        delta_y = constantes.VELOCIDAD
+
+    print(f"{delta_x},{delta_y}")
+
+    #Mover al jugador
+    jugador.movimiento(delta_x, delta_y)
+
+   
     jugador.dibujar(ventana)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False 
-    
+
+        #TECLADO
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                mover_izquierda = True
+            if event.key == pygame.K_w:
+                mover_arriba = True
+            if event.key == pygame.K_d:
+                mover_derecha = True
+            if event.key == pygame.K_s:
+                mover_abajo = True
+
+        #Para marcar cuando soltamos la tecla
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                mover_izquierda = False
+            if event.key == pygame.K_w:
+                mover_arriba = False
+            if event.key == pygame.K_d:
+                mover_derecha = False
+            if event.key == pygame.K_s:
+                mover_abajo = False
+
+
     pygame.display.update()
 
 pygame.quit()           
